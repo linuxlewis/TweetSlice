f249c444a38795f2cd25e1f9f8c3cb5fb5978dd9
Sam Bolgert <sam@livelovely.com>
2013-01-30 19:08:44 -0800

    test to email

---
+++ tweetslice/app.py
@@ -0,0 +1,167 @@
+import time
+import smtplib
+from twitter import *
+from datetime import datetime
+import re
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
+from email.utils import formataddr, formatdate
+from jinja2 import Environment, PackageLoader
+
+
+consumer_key = 'Wx2lGU5aLowSXDxFNmMBzg'
+consumer_secret = 'pF9BYbosXaRZQaIORc4KNviY29LbtOBdEyfJmaAXnwA'
+
+oauth_token = '1135522998-RMEw1Z7MSmv3yWkFM3TszQX0MzWdkGOpmykVw1e'
+oauth_secret = 'ZxZ1ey5qyoUmpV5etMXfm3CktukaM2mVdCAkxIGgo'
+
+twitter_keys = []
+app_key = 'blah'
+sendgrid_key = 'ok'
+
+types = ['washer', 'dryer', 'toliet', 'sink', 'dishwasher', 'broken']
+
+user = {'email':'sam@livelovely.com', 'name':'Sam Bolgert'}
+
+
+
+def run():
+	print 'Starting TweetSlice'
+	#start threads
+	while True:
+		last_tweet = None
+
+		#create twitter
+		t = Twitter(
+			auth=OAuth(oauth_token, oauth_secret,
+				consumer_key, consumer_secret)
+		)
+		tweets = t.statuses.mentions_timeline()
+
+		for tweet in tweets:
+			print 'Checking Tweets'
+			#if tweet is new
+			created_at = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+			if last_tweet is None or created_at > last_tweet:
+				check_tweet_for_maintaince(tweet)
+				last_tweet = created_at
+		time.sleep(60)
+
+def check_tweet_for_maintaince(tweet):
+	main = False
+	for hash in tweet['entities']['hashtags']:
+		if hash['text'] == 'maintenance':
+			print 'Maintenance Tweet Found!'
+			main = True
+			break
+
+	#search for other types
+	if main is True:
+		for type in types:
+			match = re.search(type, tweet['text'])
+			if match is not None:
+				send_email_to_user(user, type, tweet)
+				break
+
+def send_email_to_user(user, type, tweet):
+	print 'Sending Email'
+	c = dict()
+	c['type'] = type
+	c['name'] = tweet['user']['name']
+	c['text'] = tweet['text']
+	env = Environment(loader=PackageLoader('tweetslice', 'templates'))
+	tmp = env.get_template('general.html')
+	html = tmp.render({'c':c})
+	#html = render('/messages/alert.html')
+	_sendHtmlEmail('maintenance@livelovely.com', user['email'], "New Maintenance request from twitter user:" + tweet['user']['screen_name'] ,None, html, 'Lovely Maintenance', user['name'])
+
+def _sendHtmlEmail(fromAddress, toAddress, title, textMessage, htmlMessage, fromName=None, toName=None, attachments=None, ignoreOverride=False, sender=None):
+
+	header_charset = 'ISO-8859-1'
+	bodyCharsets = 'US-ASCII', 'ISO-8859-1', 'UTF-8'
+
+	#Encode to and From Names
+	if fromName is not None:
+		fromName = str(Header(unicode(fromName), header_charset))
+
+	if toName is not None:
+		toName = str(Header(unicode(toName), header_charset))
+
+	#Encode the text and html bodies
+	if textMessage is not None:
+		for textCharset in bodyCharsets:
+			try:
+				textMessage.encode(textCharset)
+			except UnicodeError:
+				pass
+			else:
+				break
+
+	for htmlCharset in bodyCharsets:
+		try:
+			htmlMessage.encode(htmlCharset)
+		except UnicodeError:
+			pass
+		else:
+			break
+
+	#Encode the email addresses
+	fromAddress = fromAddress.encode('ascii')
+	toAddress = toAddress.encode('ascii')
+
+	#Create the message
+	message = MIMEMultipart("alternative")
+
+	#Add the encoded subject
+	message["Subject"] = Header(unicode(title), header_charset)
+
+	#Add the Date Header
+	message["Date"] = formatdate(localtime=True)
+
+	#Add the from address
+	if fromName is None:
+		message["From"] = fromAddress
+	else:
+		message["From"] = formataddr((fromName, fromAddress))
+
+	#Add the to address
+	if toName is None:
+		message["To"] = toAddress
+	else:
+		message["To"] = formataddr((toName, toAddress))
+
+	#do the headers
+	if sender is not None:
+		message.add_header('Sender', sender)
+		message.add_header('Return-Path', sender)
+
+	#Add the text body
+	if textMessage is not None:
+		textPart = MIMEText(textMessage.encode(textCharset), 'plain', textCharset)
+		message.attach(textPart)
+
+	#Add the html body
+	htmlPart = MIMEText(htmlMessage.encode(htmlCharset), 'html', htmlCharset)
+	message.attach(htmlPart)
+
+	#Add the attachments
+	if attachments is not None:
+		for attachment in attachments:
+			a = MIMEApplication(attachment[0])
+			a.add_header('Content-Disposition', 'attachment', filename=attachment[1])
+			message.attach(a)
+
+	#Send the message
+	session = smtplib.SMTP('smtp.sendgrid.net')
+	session.login('doug@homeboodle.com', '88gg88')
+	smtpResult = session.sendmail(fromAddress, toAddress, message.as_string())
+
+	print 'made it'
+
+	#Quit the session
+	session.quit()
+
+if __name__ == '__main__':
+	run()
